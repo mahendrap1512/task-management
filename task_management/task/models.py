550502b23ba8bde362b7337c2a5896a65b2ffede
mahendra <mahendrap1512@gmail.com>
2021-06-12 13:55:57 +0530

    Add Task model

--- task_management/task/models.py
+++ task_management/task/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from common.base_model import BaseModel
+from project.models import Project
 
-# Create your models here.
+
+class Task(BaseModel):
+
+    class TaskStatus(models.TextChoices):
+        TODO = "TODO"
+        WIP = "WIP"
+        ONHOLD = "ONHOLD"
+        DONE = "DONE"
+
+    name = models.CharField(max_length=64, blank=False, null=False)
+    description = models.CharField(max_length=64, blank=True, null=True)
+    project = models.ForeignKey(Project, related_name="tasks", on_delete=models.DO_NOTHING)
+    status = models.CharField(max_length=20, choices=TaskStatus.choices, default=TaskStatus.TODO.value)
+
+    def __str__(self) -> str:
+        return self.name
+
+    class Meta:
+        db_table = "Task"
